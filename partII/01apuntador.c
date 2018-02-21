eb7c010dc2e03421a96ab3da4033cabbeb00df08
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2018-02-20 20:33:02 -0600

    Update 01apuntador.c

--- partII/01apuntador.c
+++ partII/01apuntador.c
@@ -1,27 +1,22 @@
-/*
-Veamos otro operador de direcciones de memoria
-*/
-
 #include <stdio.h>
 
-void intercambio(int *a, int *b){ 
-int aux = *a;
-*a=*b; 
-*b=aux;
+int main () {
+
+ /* definicion de la variable */
+   int  var = 20;
+   /* declaracion de la variable apuntador */  
+   int  *ip;
+
+/* guardar direccion de var en la variable apuntador*/
+   ip = &var;  
+
+   printf("La direccion de la variable var es: %x\n", &var  );
+
+   /* Direccion guarda en la variable apuntador*/
+   printf("La direccion guardada en la variable  ip es: %x\n", ip );
+
+   /* Aceder al valor usando el apuntador */
+   printf("Valor de la variable *ip: %d\n", *ip );
+
+   return 0;
 }
-
-int main(){
-int i=3, j=50;
-
-printf("i=%d y j=%d\n", i,j); 
-intercambio(&i, &j); 
-
-printf("i=%d y j=%d\n", i,j);
-
-return 0; 
-}
-
-/*
-El operador * lee o modifica el valor apuntado
-
-*/
