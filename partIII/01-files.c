2fdd0153a38766eb5327f9ab7c86f20e1982d17e
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2018-03-20 22:01:45 -0600

    Update 01-files.c

--- partIII/01-files.c
+++ partIII/01-files.c
@@ -1,37 +1,20 @@
-/*
-
-En este programa se crea crea un archivo, guarda información y luego la imprime en 
-pantalla.
-
-Hablaremos de las funciones en clase.
-
-
- 
-*/
-
 
 #include <stdio.h>
+#include <stdlib.h>
+ 
+int main () 
+{
+  FILE * archivo;
+  
+  char nom[] = "licencia.est";
+  
+
+  archivo = fopen ( nom, "a" );
+  
+ 
+  fclose ( archivo );
 
 
-int main () {
-   char str1[7], str2[2], str3[2];
-   int a;
-   FILE *arc_impr;
-
-   arc_impr = fopen ("file.txt", "w+");
-   fputs("Estamos en el 2017", arc_impr);
-   
-   rewind(arc_impr);
-   fscanf(arc_impr, "%s %s %s %d", str1, str2, str3, &a);
-   
-   printf("Imprime cadena: |%s| |%s| |%s| |%d| \n", str1, str2, str3, a);
-
-   fclose(arc_impr);
-   
-   return(0);
+  return 0;
 }
-
-/*
-Investiga para qué sirve la función rewind()
-*/
 
