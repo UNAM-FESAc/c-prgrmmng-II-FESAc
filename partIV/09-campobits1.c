6b23f759935a194a5c20dade4c289ca03fadc4e2
J. A. Orduz-Ducuara <jaorduz@ciencias.unam.mx>
2018-04-18 08:55:28 -0500

    Add files via upload

---
+++ partIV/09-campobits1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+struct {
+// El numero indica cuantos bits se permiten 00000000. Inicia de der-izq
+   unsigned int age : 3;
+// Cambia el numero y observa lo que sucede.
+} Age;
+
+int main( ) {
+
+   Age.age = 4;
+   printf( "Sizeof( Age ) : %d\n", sizeof(Age) );
+
+   printf( "Age.age : %d\n", Age.age );
+
+   Age.age = 7;
+   printf( "Age.age : %d\n", Age.age );
+
+   Age.age = 8;
+   printf( "Age.age : %d\n", Age.age );
+
+   return 0;
+}
